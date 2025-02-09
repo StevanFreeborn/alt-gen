92c0bee83bd9b265a4dd4b147d13ff1e4cfe4da8
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-02-09 15:54:48 -0600

    feat: begin working on console app

---
+++ src/AltGen.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿// TODO: We want to be able
+// to provide a path to an image
+// read the image
+// and then post the image to our API
+// get the response and display the alt text
+
+using Microsoft.Extensions.Hosting;
+
+await Host.CreateDefaultBuilder(args)
+  .ConfigureServices(static (_, services) => { })
+  .Build() // TODO: Need special extension for integrating host with Spectre.Console
+  .RunAsync();
+
+Console.WriteLine("Hello, World!");
